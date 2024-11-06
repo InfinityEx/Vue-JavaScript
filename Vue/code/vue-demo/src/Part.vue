4c7abd780b9456d76f38f5b19bb15f11199690f5
時無_ShiWu <756423078@qq.com>
2024-11-06 14:37:34 +0800

    merge folder from VirtualMachine

---
+++ Vue/code/vue-demo/src/Part.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="ass">
+        <p>Show me your everything!</p>
+    </div>
+    <p>Vue3</p>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style>
+.ass{
+    font-weight: bold;
+    font-size: 20px;
+}
+</style>
---
+++ Vue/code/vue-demo/src/components/vue2button.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <h2></h2>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                name:"Linus Andrew",
+                age:24,
+                tel:"16822658894"
+            }
+            
+        }
+    }
+</script>
